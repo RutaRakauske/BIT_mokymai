e703d9a3ece005dd08398f845ddf85054b754cbd
Ruta Rakauske <mikutyte.ruta@gmail.com>
2022-03-15 21:41:29 +0200

    Trying out javascript

---
+++ Week_8/JavascriptBasics/script.js
@@ -0,0 +1,58 @@
+console.log ("********** 1 Uzduotis **********");
+
+var a = 1;
+let b = 2;
+const c = 3;
+
+// Prints to console
+
+console.log (a);
+console.log (b);
+console.log (c);
+
+
+// can't redeclare same const, let, var in same function
+// const a = 2;
+// let b = 5;
+// const c = 8;
+
+console.log ("********** 2 Uzduotis **********");
+
+const vardas = "Ruta";
+const pavarde = "Rakauske";
+let amzius = 30;
+
+console.log (vardas);
+console.log (pavarde);
+console.log (amzius);
+
+// adding one more year
+amzius++;
+
+console.log (amzius);
+
+console.log ("********** 3 Uzduotis **********");
+
+const price = 25;
+let total = 10;
+const tax = 0.21;
+
+total = (tax * price) + price;
+
+console.log (price);
+console.log (total);
+console.log (tax);
+
+console.log ("********** 4 Uzduotis **********");
+
+const shuttleName = "Determination";
+const speedMph = 17500;
+const kmMars = 225000000;
+const kmMoon = 384400;
+const miPerKm = 0.621;
+
+console.log (shuttleName);
+console.log (speedMph);
+console.log (kmMars);
+console.log (kmMoon);
+console.log (miPerKm);
