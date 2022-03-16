7b014e3b44776974d82e49b2d18821ba9ebda256
Ruta Rakauske <mikutyte.ruta@gmail.com>
2022-03-16 19:22:41 +0200

    Javascript operators practice

---
+++ Week_8/JSoperators/script.js
@@ -0,0 +1,54 @@
+const sesi = 3 + 3;
+const du = 3 - 1;
+const desimt = 2 * 5;
+const penki = 40 / 8;
+const vienas = 7 % 3;
+let trys = 2;
+trys++;
+let keturi = 5;
+let sesiolika = 2**4;
+let astuoni = 6;
+let devyni = 10;
+let dvylika = 6;
+let trylika = 26;
+let two = 5;
+let keturiolika = 7;
+const a = 5;
+const b = 4;
+const c = 5;
+const d = "tekstas";
+
+console.log (sesi);
+console.log (du);
+console.log (desimt);
+console.log (penki);
+console.log (vienas);
+console.log (trys);
+console.log (--keturi);
+console.log (sesiolika);
+console.log (astuoni +=2);
+console.log (devyni -=1);
+console.log (dvylika *=2);
+console.log (trylika /=2);
+console.log (two %=3);
+console.log (keturiolika *=2);
+console.log (a==b);
+console.log (a===c);
+console.log (a!=c);
+console.log (a!==d);
+console.log (a>b);
+console.log (a>=c);
+console.log (a>=c && b>c);
+console.log (a>=c || b>c);
+console.log (!(b>c));
+console.log (typeof(2+4));
+console.log (typeof("tekstas"));
+
+
+const x = 10, y = 5;
+
+const z = x > y? x : y; // if true, then x, if false then y. Value of z would be 10
+const w = x > y? y : x; // if true, then x, if false then y. Value of w would be 5
+
+console.log (z);
+console.log (w);
