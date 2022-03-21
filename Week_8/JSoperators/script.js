68e252cd7cb2ba12476dd56fc27814a45c9dcf0e
Ruta Rakauske <mikutyte.ruta@gmail.com>
2022-03-21 21:14:58 +0200

    reverted

--- Week_8/JSoperators/script.js
+++ Week_8/JSoperators/script.js
@@ -1,205 +1,69 @@
-//  1 uzduotis
-// Apibrėžkite funkcija pavadinimu hello(), kurią pašaukus, atspausdintų konsolėje "Hello World"
-console.log("********** 1 uzduotis ***********")
+const sesi = 3 + 3;
+const du = 3 - 1;
+const desimt = 2 * 5;
+const penki = 40 / 8;
+const vienas = 7 % 3;
+let trys = 2;
+trys++;
+let keturi = 5;
+let sesiolika = 2**4;
+let astuoni = 6;
+let devyni = 10;
+let dvylika = 6;
+let trylika = 26;
+let two = 5;
+let keturiolika = 7;
+const a = 5;
+const b = 4;
+const c = 5;
+const d = "tekstas";
 
-hello();
+console.log (sesi);
+console.log (du);
+console.log (desimt);
+console.log (penki);
+console.log (vienas);
+console.log (trys);
+console.log (--keturi);
+console.log (sesiolika);
+console.log (astuoni +=2);
+console.log (devyni -=1);
+console.log (dvylika *=2);
+console.log (trylika /=2);
+console.log (two %=3);
+console.log (keturiolika *=2);
+console.log (a==b);
+console.log (a===c);
+console.log (a!=c);
+console.log (a!==d);
+console.log (a>b);
+console.log (a>=c);
+console.log (a>=c && b>c);
+console.log (a>=c || b>c);
+console.log (!(b>c));
+console.log (typeof(2+4));
+console.log (typeof("tekstas"));
+console.log (typeof(4>2));
+console.log (typeof(4>"2"));
+console.log (typeof(4 + "2"));
 
-function hello() {
-  console.log("Hello World");
-}
 
-//  2 uzduotis
-// Patobulinkite prieš tai parašyta funkcija hello(), kuri priimtų parametrą vardas, ir ją pašaukus, išspausdintų konsolėje "Hello ${vardas}.". Pašaukite funkcija du kartus, kad išspausdintų jūsų vardą, o vėliau jūsų mėgstamo aktoriaus vardą. 
-console.log("********** 2 uzduotis ***********")
+const x = 10, y = 5;
+const z = x > y? x : y; // if true, then x, if false then y. Value of z would be 10
+const w = x > y? y : x; // if true, then x, if false then y. Value of w would be 5
 
-helloWorld("Ruta");
-helloWorld("Tom");
+console.log (z);
+console.log (w);
 
-function helloWorld(vardas) {
-  console.log("Hello", vardas);
-}
+const arlyginis = 10 % 2 === 0;
+console.log (arlyginis);
 
-//  3 uzduotis
-// Sukurkite du kintamuosiu vardas ir amžius. Žemiau apibrėžkite funkcija prabegoMetai(), kuri kiekviena karta pašaukta pakeltų metus per vienetą ir konsolėje atspausdintų jūsų vardą ir metus. Pamėginkite prabėgti kelis metus iš eilės. 
-console.log("********** 3 uzduotis ***********")
+// Prefix increment
+let prefix = 1;
+console.log(++prefix); // 2
+console.log(prefix); // 2
 
-const vardas = "Ruta";
-let amzius = 20;
-
-function  prabegoMetai(){
-  ++amzius;
-}
-
-prabegoMetai();
-console.log(amzius);
-prabegoMetai();
-console.log(amzius);
-prabegoMetai();
-console.log(amzius);
-
-//  4 uzduotis
-// Apibrėžkite funkcija pavadinimu fizzAndBazz(), kuri priimtų viena parametra string ir ją pašaukus su argumentu "fizz",  konsolėje atspausdintų "bazz", bet ją pašaukus su parametru "bazz", tuomet konsolėje atspausdintų "fizz", kitu atveju spausdintų "Please provide fizz or bazz" 
-console.log("********** 4 uzduotis ***********")
-
-fizzAndBazz("kazkas");
-fizzAndBazz("bazz");
-
-function fizzAndBazz(word){
-  switch(word) {
-    case "fizz":
-      console.log("bazz");
-      break;
-    case "bazz":
-      console.log("fizz");
-      break;
-    default:
-      console.log("Please provide fizz or bazz");
-  }
-}
-
-//  5 uzduotis
-// Apibrėžkite funkcija kuri priimtų tris parametrus numerius ir konsolėje atspausdintų mažiausią numerį. 
-console.log("********** 5 uzduotis ***********")
-
-numbers(1,1,3);
-numbers(1,2,3);
-
-function numbers(number1, number2, number3){
-  if (number1<number2 && number1<number3) {
-    (console.log("maziausias numeris -", number1));
-  }
-  else if (number2<number1 && number2<number3) {
-    (console.log("maziausias numeris -", number2));
-  } 
-  else if (number3<number1 && number3<number2) {
-    (console.log("maziausias numeris -", number3));
-  }
-  else {
-    console.log("visi skaiciai turi buti skirtingi");
-  }
-}
-
-//  6 uzduotis
-// Apibrėžkite funkcija kuri priimtų tris parametrus numerius ir konsolėje atspausdintų jų aritmetinį vidurkį.
-console.log("********** 6 uzduotis ***********")
-
-aritmetinisVid(1, 0, 3);
-aritmetinisVid(1, 254, 3);
-
-function aritmetinisVid(no1, no2, no3){
-  let vidurkis = (no1 + no2 + no3)/3;
-  console.log(vidurkis);
-}
-
-//  7 uzduotis
-// Apibrėžkite funkcija kuri priimtų parametra numerį ir grąžintų vienetu aukštesnę vertę. Priskirkite grąžinimo rezultatą kintamąjam ir išspausdinkite kintamojo reikšmę ekrane. 
-console.log("********** 7 uzduotis ***********")
-
-function onePlus(no1){
-  return no1 = no1 + 1;
-}
-console.log(onePlus(2));
-
-//  8 uzduotis
-// Apibrėžkite funkcija kuri priimtų du parametrus tekstus A ir B ir patikrintų kuris tekstas turi daugiau raidžių. ("labas".length)Na Išspausdinkite masyvo pavadinimą (A arba B) ir jo narių skaičių ekrane.
-console.log("********** 8 uzduotis ***********")
-
-function raides(A, B){
-  if (A.length > B.length){
-    console.log("A - ", A.length);
-  }
-  else if (B.length > A.length){
-    console.log("B -", B.length);
-  }
-  else {
-    console.log("iveskite skirtingo ilgio zodzius");
-  }
-}
-raides("pirmas", "antras");
-raides("dvidesimt", "antras");
-
-//  9 uzduotis
-//  Ši funkcija turi grąžinti true jei x yra lygus 7.
-console.log("********** 9 uzduotis ***********")
-
-function isSeven(x) {
-    return x == 7? true:false;
-}
-console.log(isSeven(7));
-console.log(isSeven(10));
-
-//  10 uzduotis
-// Apibrėžkite funkcija isEqual, kuri priimtu du parametrus ir grąžintų boolean true jeigu abu parametrai yra strict lygūs arba false jei ne. Pasitikrinkite funkcijos veikimą su console.log.
-// isEqual(1, true) ➞ false
-// isEqual(1,  1) ➞ true
-console.log("********** 10 uzduotis ***********")
-
-function isEqual(x, y){
-  if (x===y) {
-    return bool = true;
-  }
-  else {
-    return bool = false;
-  }
-}
-console.log(isEqual(1, true));
-console.log(isEqual(1, 1));
-
-//  11 uzduotis
-// Apibrėžkite funkcija kuri priimtų tris parametrus prob, prize, pay ir grąžintų true jeigu prob padauginus iš prize  yra daugiau už pay, kitu atveju grąžintų false. 
-console.log("********** 11 uzduotis ***********")
-
-function price(prob, prize, pay){
-  if ((prob * prize) > pay) {
-    return bool = true;
-  }
-  else {
-    return bool = false;
-  }
-} 
-
-console.log(price(2,10,19));
-console.log(price(2,3,19));
-
-//  12 uzduotis
-// Apibrėžkite funkcija kuri  priimtu parametrą minutes ir grąžintų vertę sekundėmis. Sukurkite kelis kintamuosius  šaukiant šią funkciją. Vėliau atspausdinkite kintamųjų reikšmes konsolėje.  
-console.log("********** 12 uzduotis ***********")
-
-function convertMinutes(minutes){
-  let seconds = minutes * 60;
-  return seconds;
-}
-
-const halfMin = convertMinutes(0.5);
-console.log(halfMin);
-const five = convertMinutes(5);
-console.log(five);
-const sixtyFive = convertMinutes(65);
-console.log(sixtyFive);
-
-//  13 uzduotis
-// Apibrėžkite funkcija kuri priimtų numerį metus ir atspausdintų konsolėje ar tai lyginiai metai ar ne. 
-console.log("********** 13 uzduotis ***********")
-
-function evenYear(year){
-  if (year % 2 === 0){
-    console.log("Lyginis");
-  }
-  else {
-    console.log("Nelyginis")
-  }
-}
-evenYear(2002);
-evenYear(2001);
-
-//  14 uzduotis
-//     Apibrėžkite funkcija kuri priimtų du parametrus, nuvažiuoti kilometrai ir benzino litro kaina, ir apskaičiuotų kiek jums kainavo kelionė.  Automobilio kuro vartojimą priskirkite kintamajam, o jo vertę pasirinkite pagal jūsų turimo automobilio kuro vartojimą.
-console.log("********** 14 uzduotis ***********")
-
-function fuelPrice(km, price){
-  const consumption = 5;
-  let total = km/(100/consumption) * price;
-  return total;
-}
-
-console.log(fuelPrice (200, 1.80));
+// Postfix increment
+let postfix = 1;
+console.log(postfix++); // 1
+console.log(postfix); // 2
