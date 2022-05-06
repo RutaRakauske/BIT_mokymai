26b6854e611e73341e4816081ba643653d548634
Ruta Rakauske <mikutyte.ruta@gmail.com>
2022-05-06 09:44:37 +0300

    navigation with node JS task

--- Week_15/2022-05-05/index.js
+++ Week_15/2022-05-05/index.js
@@ -1,16 +1,37 @@
 import express from 'express'
-
+import {dirname} from 'path'
+import {fileURLToPath} from 'url'
+// Express modulio iniciavimas
 const app = express()
+// sugeneruojamas esamos direktorijos absoliutus kelias
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // Kelias/route
 
 app.get('/', (request, response) => {
     // response.status(404) kaip siųsti statuso kodą
-    response.send('<h1>Titulinis puslapis</h1>')
+    // response.send('<h1>Titulinis puslapis</h1>')
+    response.sendFile(__dirname + '/templates/index.html')
 })
 
-app.get('/apie-mus', (request, response) => {
-    response.send('<h1>Apie mus</h1>')
+app.get('/about', (request, response) => {
+    // response.send('<h1>Apie mus</h1>')
+    response.sendFile(__dirname + '/templates/about.html')
+})
+
+app.get('/career', (request, response) => {
+    // response.send('<h1>Apie mus</h1>')
+    response.sendFile(__dirname + '/templates/career.html')
+})
+
+app.get('/clients', (request, response) => {
+    // response.send('<h1>Apie mus</h1>')
+    response.sendFile(__dirname + '/templates/clients.html')
+})
+
+app.get('/contacts', (request, response) => {
+    // response.send('<h1>Apie mus</h1>')
+    response.sendFile(__dirname + '/templates/contacts.html')
 })
 
 app.listen(3000)
